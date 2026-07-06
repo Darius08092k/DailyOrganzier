88783960b49697131418e185ccf6f0c2394e48dc
Darius Ionica <dariusionica2000@gmail.com>
2026-07-06 20:07:20 +0300

    Revert

--- DailyOrganzier/Views/AddQuestPopup.xaml.cs
+++ DailyOrganzier/Views/AddQuestPopup.xaml.cs
@@ -1,7 +1,6 @@
-using CommunityToolkit.Maui.Views;
-using DailyOrganzier.Models;
-using DailyOrganzier.ViewModels;
 using System;
+using DailyOrganzier.Models;
+using CommunityToolkit.Maui.Views;
 
 namespace DailyOrganzier.Views;
 
@@ -11,19 +10,38 @@ public partial class AddQuestPopup : Popup<Quest>
     public AddQuestPopup()
     {
         InitializeComponent();
-
-        var viewModel = new AddQuestPopupViewModel();
-        viewModel.ClosePopupAction = async (quest) =>
-        {
-            await CloseAsync(quest);
-        };
-        BindingContext = viewModel;
     }
 
     private async void OnCancelClicked(object sender, EventArgs e)
     {
         await CloseAsync(null);
     }
+    // TODO: Add validation for the TitleEntry and XpEntry fields, and display an error message if the input is invalid.
+    // TODO: Add a check to ensure that the XpEntry field is a valid integer, and display an error message if it is not.
+    // TODO: Add a check to ensure that the XpEntry field is not negative, and display an error message if it is.
+    // TODO: Add a check to ensure that the TitleEntry field is not empty, and display an error message if it is.
+    // TODO: Add a chekc to ensure that the XP is not too high, and display an error message if it is.
+    private async void OnSaveClicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(TitleEntry.Text))
+        {
+            return;
+        }
 
-    
+        int xp = 10;
+        if (!string.IsNullOrWhiteSpace(XpEntry.Text) && int.TryParse(XpEntry.Text, out int parsedXp))
+        {
+            xp = parsedXp;
+        }
+
+        var newQuest = new Quest
+        {
+            Id = Guid.NewGuid().ToString(),
+            Title = TitleEntry.Text,
+            Type = CategoryPicker.SelectedItem.ToString(),
+            XpReward = xp
+        };
+
+        await CloseAsync(newQuest);
+    }
 }
